08c23b9df813e737a0ebb42ec349139221df57fa
Hackzer422 <fernandofcamacho22@gmail.com>
2022-10-05 17:18:06 -0300

    de vuelta en code

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato (nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const ppnombre={
+    usernombre:nombre, 
+    useredad: edad,
+    devolverMeow: function()
+    {
+      console.log('MEOW!');
+    }
+  };
+  ppnombre.devolverMeow();
 }
 
 function agregarPropiedad (objeto, property) {
@@ -13,6 +22,8 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto[property]=null;
+  return objeto;
 }
 
 function invocarMetodo (objeto, metodo) {
@@ -20,13 +31,20 @@ function invocarMetodo (objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+ const objeto={
+  mmetodo:function(){
+  }
+ };
+ objeto.mmetodo();
 }
 
 function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  var produ;
+  produ=objetoMisterioso['numeroMisterioso']*5;
+  return produ;
 }
 
 function eliminarPropiedad (objeto, unaPropiedad) {
@@ -34,19 +52,33 @@ function eliminarPropiedad (objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad];
+  return objeto;
 }
 
 function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  const usernuevo={
+    username:nombre,
+    useremail:email,
+    userpass:password
+  };
+  return usernuevo;
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  for(let calve in usuario)
+  {
+    if(calve==='email')
+    return true;
+    else
+    return false;
+  }
 }
 
 function tienePropiedad (objeto, propiedad) {
@@ -54,6 +86,16 @@ function tienePropiedad (objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  for(let clave in objeto)
+  {
+    if(objeto[clave]=='key')
+    {
+      if(objeto[clave]===propiedad)
+      return true;
+      else
+      return false;
+    }
+  }
 }
 
 function verificarPassword (usuario, password) {
@@ -61,12 +103,18 @@ function verificarPassword (usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(usuario.password === password)
+  return true
+  else
+  return false;
 }
 
 function actualizarPassword (usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password=nuevaPassword;
+  return usuario;
 }
 
 function agregarAmigo (usuario, nuevoAmigo) {
@@ -74,6 +122,10 @@ function agregarAmigo (usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // Tu código:
+  const arreglo = usuario['amigos'];
+  arreglo.push(nuevoAmigo);
+  usuario['amigos']=arreglo;
+  return usuario;
 }
 
 function pasarUsuarioAPremium (usuarios) {
@@ -82,6 +134,19 @@ function pasarUsuarioAPremium (usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
+  // usuarios[user1, user2,user3]
+  const varr=[usuarios];
+  
+  for(var i=0;i<varr.length;i++)
+  {
+    for(let calve in usuario)
+    {
+      if(calve==='esPremium')
+      {
+        usuario.esPremium=true;
+      }
+    }
+  } 
 }
 
 function sumarLikesDeUsuario (usuario) {
